3608c760fc95fa8ffbe54af440463cf6a4c44e8c
tho <nguyenducthodn123@gmail.com>
2024-09-11 17:23:51 +0700

    Updates

---
+++ precache-manifest.de8bcd243c77a55c23e28e43b2e7590c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "896506cba8be3375584a",
+    "url": "/lab23/static/css/main.d3871205.chunk.css"
+  },
+  {
+    "revision": "896506cba8be3375584a",
+    "url": "/lab23/static/js/main.896506cb.chunk.js"
+  },
+  {
+    "revision": "09900e8bcdd31a54a158",
+    "url": "/lab23/static/js/1.09900e8b.chunk.js"
+  },
+  {
+    "revision": "0abb5e5dc43bdba6c4c8",
+    "url": "/lab23/static/js/runtime~main.0abb5e5d.js"
+  },
+  {
+    "revision": "c6e03a78644a5d8034c00564c5bfc959",
+    "url": "/lab23/index.html"
+  }
+];
